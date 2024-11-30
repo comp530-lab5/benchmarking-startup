160117b8081ab51f8af72f23c458aa8d7694a7e3
Sarah Haddix <sarahbh@unc.edu>
2024-11-30 15:57:26 +0000

    changed name of file

--- update_remote.sh
+++ update_remote.sh
@@ -4,4 +4,4 @@
 #scp -o ProxyJump=sarahbh@login.cs.unc.edu Makefile sarahbh@adams.cs.unc.edu:
 #/scp -o ProxyJump=sarahbh@login.cs.unc.edu unit_tests.py sarahbh@adams.cs.unc.edu:
 
-scp -o ProxyJump=sarahbh@login.cs.unc.edu unit_tests.py ttest.py Makefile benchmark_tests.c sarahbh@adams.cs.unc.edu:
+scp -o ProxyJump=sarahbh@login.cs.unc.edu unit_tests.py generate_results.py Makefile benchmark_tests.c sarahbh@adams.cs.unc.edu:
